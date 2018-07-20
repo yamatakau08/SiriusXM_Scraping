a21ded889d776b78f101152d5095ee90eb603113
yamatakau08 <yamatakau08@gmail.com>
2018-07-20 19:18:55 +0900

    add the process to get the station list of "Music" view-all

--- Selenium_SiriusXM.rb
+++ Selenium_SiriusXM.rb
@@ -38,6 +38,9 @@ else
   sleep stime
 end
 
+# https://seleniumjp.slack.com/archives/C04HD4JRF/p1532060248000014
+driver.switch_to.window(driver.window_handles.last)
+
 # url: https://player.siriusxm.com/welcome
 # click "Watch and Listen Now"
 item_str = "Watch and Listen Now"
@@ -47,11 +50,76 @@ item  = driver.find_element(:class, cname)
 if item
   puts "[INFO] \"#{item_str}\" found"
   puts "[INFO] \"#{item_str}\" click"
-  #  item.click
+  item.click
   puts "[INFO] \"#{item_str}\" clicked"
 else
   puts "[ERROR] \"#{item_str}\" not found"
   sleep stime
 end
+
+# url: https://player.siriusxm.com/home/foryou
+item_str = "Music"
+puts "[CHECK] \"#{item_str}\""
+cname = "super-category-btn"
+items = driver.find_elements(:class, cname)
+items.each { |item| puts "[DEBUG] #{item.text}" }
+item = items.find { |item| item.text.include?(item_str) }
+if item
+  puts "[INFO] \"#{item_str}\" found"
+  puts "[INFO] \"#{item_str}\" click"
+  item.click
+  puts "[INFO] \"#{item_str}\" clicked"
+else
+  puts "[ERROR] \"#{item_str}\" not found"
+  sleep stime
+end
+
+# url: https://player.siriusxm.com/home/foryou
+item_str = "View All"
+puts "[CHECK] \"#{item_str}\""
+cname = "header__view-all-text"
+item = driver.find_element(:class, cname)
+if item
+  puts "[INFO] \"#{item_str}\" found"
+  puts "[INFO] \"#{item_str}\" click"
+  item.click
+  puts "[INFO] \"#{item_str}\" clicked"
+else
+  puts "[ERROR] \"#{item_str}\" not found"
+  sleep stime
+end
+
+cstr = "" # cstr: content string
+# url: https://player.siriusxm.com/view-all/.+
+puts "[INFO] scraping class center-column"
+cname = "center-column"
+items = driver.find_elements(:class, cname)
+items.each do |item|
+  aria_label = item.attribute('aria-label')
+  x = aria_label.split("\n").map { |data| data.strip }
+  y = x.delete_if { |item| item.length == 0 }
+  if y.size <=4
+    station_name,ch,track,genre = y
+  else
+    station_name,ch,track,genre = y[1..4]
+  end
+
+cstr += <<"EOS"
+    {station_name: "#{station_name}",
+     ch:           "#{ch}",
+     genre:        "#{genre}"},
+EOS
+end
+
+# list up
+fstr = <<"EOS" # fstr: final string
+module SiriusXM
+  TBL_Sirius_Music = [
+#{cstr}
+  ]
+end
+EOS
+
+puts fstr
 
 sleep stime
